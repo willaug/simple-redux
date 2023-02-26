b143840499071d02947a9c4cc5e584a7580243ca
willaug <william.santos315@outlook.com>
2023-02-25 23:46:10 -0300

    feat: redux actions

--- src/components/Interval.jsx
+++ src/components/Interval.jsx
@@ -1,18 +1,27 @@
 import { connect } from 'react-redux'
+import { incrementMax, incrementMin } from '../store/numbersReducer'
 import { Card } from './Card'
 import './Interval.css'
 
-export const component = ({ min, max }) => {
+export const component = ({ min, max, changeMin, changeMax }) => {
   return (
     <Card title="Interval" color="red">
       <div className="interval">
         <div className="interval__section">
           <p>Min</p>
-          <input type="number" value={min} readOnly />
+          <input 
+            type="number" 
+            value={min}
+            onChange={(e) => changeMin(e.target.value) }
+          />
         </div>
         <div className="interval__section">
           <p>Max</p>
-          <input type="number" value={max} readOnly />
+          <input 
+            type="number" 
+            value={max}
+            onChange={(e) => changeMax(e.target.value) }
+          />
         </div>
       </div>
     </Card>
@@ -23,4 +32,9 @@ const mapStateToProps = (state) => ({
   ...state.numbers
 })
 
-export const Interval = connect(mapStateToProps)(component)
+const mapDispatchToProps = (dispatch) => ({
+  changeMin(value) { dispatch(incrementMin(value)) },
+  changeMax(value) { dispatch(incrementMax(value)) }
+})
+
+export const Interval = connect(mapStateToProps, mapDispatchToProps)(component)
--- src/store/numbersReducer.js
+++ src/store/numbersReducer.js
@@ -5,8 +5,16 @@ export const numbersSlice = createSlice({
   initialState: {
     min: 0,
     max: 10
+  },
+  reducers: {
+    incrementMin: (state, action) => {
+      state.min = action.payload;
+    },
+    incrementMax: (state, action) => {
+      state.max = action.payload;
+    }
   }
 })
 
-
+export const { incrementMin, incrementMax } = numbersSlice.actions
 export const numbersReducer = numbersSlice.reducer
